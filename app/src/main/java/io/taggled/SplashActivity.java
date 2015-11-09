46f43edfa0bdefcdb22d8c99a5e9967a9a58f92e
cuongutd <cuongutd@gmail.com>
2015-11-08 21:40:49 -0800

    just small checkin

--- app/src/main/java/io/taggled/SplashActivity.java
+++ app/src/main/java/io/taggled/SplashActivity.java
@@ -22,10 +22,13 @@ public class SplashActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Ken's  action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+
+
     }
 
     @Override
